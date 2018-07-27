964d7f8affa4b4ea56b688ec9c27b5128f672363
Louis Antoine Otter <louis.otter@gmail.com>
2018-07-27 23:57:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
-var topic = ''${topic}.'';
+var topic = '${topic}.';
 
 var conversation = `Let\'s talk about ${topic}`;
